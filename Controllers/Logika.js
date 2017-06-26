17f2d514e7d88f6a9096ff849aec857d6ab7de39
Dragan1810 <d.novakovic1810@yahoo.com>
2017-06-26 13:45:44 +0200

    extra code removed

--- Controllers/Logika.js
+++ Controllers/Logika.js
@@ -11,31 +11,26 @@ function timeConverter(UNIX_timestamp,multi){
 
 exports.tajm = (req,res) => {
     let date = req.params.date;
-    var dateBroj = Number(date);
 
-    if (isNaN(dateBroj)) {
-        console.log("nije broj nego string");  
+    if (isNaN(Number(date))) { 
         date = new Date(date);
 
-    if (date == 'Invalid Date') {
-        let time = "null";
-        res.json({ "unix": time, "natural": time });
-    }
-
-    if (Object.prototype.toString.call(date) === "[object Date]") {
-            let time = date;
-            time = timeConverter(time,1);
+        if (date == 'Invalid Date') {
+            let time = "null";
             res.json({ "unix": time, "natural": time });
+        }
 
-    } 
-} else {
+        if (Object.prototype.toString.call(date) === "[object Date]") {
+            time = timeConverter(date,1);
+            res.json({ "unix": time, "natural": time });
+        }
+    } else 
+    {
     
- console.log(Object.prototype.toString.call(dateBroj));
-    if (Object.prototype.toString.call(dateBroj) === "[object Number]") {
-        console.log("unix incoming..");
-        time = timeConverter(date,1000);
-        res.json({ "unix": time, "natural": time });
-    }
+        if (Object.prototype.toString.call(Number(date)) === "[object Number]") {
+             time = timeConverter(date,1000);
+            res.json({ "unix": time, "natural": time });
+        }
 
   
     }
